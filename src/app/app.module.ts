61fd2d978d1d3e70449ee934a9ec1b0add9993d9
Yonatan Altman <yonatan@yaltman.com>
2020-01-08 17:11:29 +0200

    works1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShareDataModule } from 'src/share-data/share-data.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShareDataModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
