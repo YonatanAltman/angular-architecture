f11120f895d7fc17be5c7902dcee142a26d2b9ee
‘Yonatan <yonatan@yaltman.com>
2020-01-22 18:17:15 +0200

    input & output

--- src/share-data/about/about.component.ts
+++ src/share-data/about/about.component.ts
@@ -10,8 +10,9 @@ export class AboutComponent implements OnInit {
   title = {
     name: 'my name is'
   }
+  isSomthing: boolean;
   constructor(public app: AppService) { }
-
+  items = ['aaaaaa', 'sssss', 'dddddd', 'gggggg', 'hhhhhh', 'a', 'a', 'a']
   ngOnInit() {
     console.log('ngOnInit About');
 
